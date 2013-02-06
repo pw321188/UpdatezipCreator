29838a21dc45dab3bde03c59b1c38e152fc24d9b
pw321188 <pw321188@students.mimuw.edu.pl>
2013-02-06 19:33:13 +0100

    author info added

--- src/main.cc
+++ src/main.cc
@@ -3,6 +3,8 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    app.setOrganizationName("Patryk Wasiewicz");
+    app.setApplicationName("UpdatezipCreator");
 
     return app.exec();
 }
